d11ef5a9676b8499739e6d7f51e24c12f8f4ef05
ahillman2000 <alexander3.hillman@live.uwe.ac.uk>
2020-10-21 14:40:55 +0100

    first commit

---
+++ Unity-portal/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void PlayerControls()
+    {
+        if (Input.GetKey("w"))
+        {
+            //print("W");
+            player.transform.Translate(0, 0, 0.1f);
+        }
+        if (Input.GetKey("a"))
+        {
+            //print("A");
+            player.transform.Translate(-0.1f, 0, 0);
+        }
+        if (Input.GetKey("s"))
+        {
+            //print("S");
+            player.transform.Translate(0, 0, -0.1f);
+        }
+        if (Input.GetKey("d"))
+        {
+            //print("D");
+            player.transform.Translate(0.1f, 0, 0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerControls();
+    }
+}
