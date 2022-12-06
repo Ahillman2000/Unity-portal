7e0864d5ce6707c0d69d77df6b5ab963138294f9
Alexander Hillman <alexander.hillman0000@gmail.com>
2022-12-06 13:31:58 +0000

    updated unity version + change to wall position

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private CharacterController characterController;
+    private PlayerInputActions playerInputActions;
+    private InputAction movement;
+    private InputAction look;
+
+    [Header("Movement")]
+    [SerializeField] private float speed = 10f;
+
+    [Header("Ground Check")]
+    [SerializeField] private Transform groundCheckObject;
+    [SerializeField] private float groundDistance = 0.4f;
+    [SerializeField] private LayerMask groundMask;
+    private bool isGrounded;
+
+    [Header("Physics")]
+    [SerializeField] private float mass = 1f;
+    [SerializeField] private float jumpHeight = 2f;
+    readonly float GRAVITY = -9.8f;
+    private Vector3 velocity;
+
+    [SerializeField] private float mouseSensitivity = 100f;
+    private float mouse_x_rotation = 0;
+
+    private void Awake()
+    {
+        playerInputActions = InputManager.Instance.playerInputActions;
+
+        movement = playerInputActions.Player.Movement;
+        look = playerInputActions.Player.Look;
+        playerInputActions.Player.Jump.performed += DoJump;
+    }
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        PlayerMovement();
+        CameraRotation();
+
+        Gravity();
+        GroundCheck();
+    }
+
+    /// <summary>
+    /// faces player in direction of camera and moves in relation to camera
+    /// </summary>
+    private void PlayerMovement()
+    {
+        transform.rotation = Quaternion.Euler(0, Camera.main.transform.eulerAngles.y, 0);
+
+        var forward = Camera.main.transform.forward;
+        var right = Camera.main.transform.right;
+
+        forward.y = 0f;
+        right.y = 0f;
+
+        forward.Normalize();
+        right.Normalize();
+
+        Vector3 moveDirection = right * movement.ReadValue<Vector2>().x + forward * movement.ReadValue<Vector2>().y;
+
+        characterController.Move(speed * Time.deltaTime * moveDirection);
+    }
+
+    /// <summary>
+    /// Rotates the player horizontally and the camera vertically
+    /// </summary>
+    private void CameraRotation()
+    {
+        float mouse_x = look.ReadValue<Vector2>().x * mouseSensitivity * Time.deltaTime;
+        float mouse_y = look.ReadValue<Vector2>().y * mouseSensitivity * Time.deltaTime;
+
+        mouse_x_rotation -= mouse_y;
+        mouse_x_rotation = Mathf.Clamp(mouse_x_rotation, -90, 90);
+
+        this.gameObject.transform.Rotate(0, mouse_x, 0);
+        Camera.main.transform.localRotation = Quaternion.Euler(mouse_x_rotation, 0, 0);
+    }
+
+    /// <summary>
+    /// Gravitational force applied to the character controller
+    /// </summary>
+    private void Gravity()
+    {
+        velocity.y += GRAVITY * mass * Time.deltaTime;
+        characterController.Move(velocity * Time.deltaTime);
+    }
+
+    /// <summary>
+    /// Check to determine if character controller is touching ground
+    /// </summary>
+    private void GroundCheck()
+    {
+        isGrounded = Physics.CheckSphere(groundCheckObject.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+    }
+
+    private void DoJump(InputAction.CallbackContext context)
+    {
+        if(isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * GRAVITY);
+        }
+    }
+}
---
+++ Assets/Scripts/Portals/SpawnPortal.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SpawnPortal : MonoBehaviour
+{
+    private PlayerInputActions playerInputActions;
+    Camera mainCamera;
+
+    [Header("Portal Prefabs")]
+    public GameObject portalLeftPrefab;
+    public GameObject portalRightPrefab;
+
+    [Header("Portal Render Target Textures")]
+    public Material portalLeftActiveMaterial;
+    public Material portalRightActiveMaterial;
+
+    // instances of each portal once created
+    [HideInInspector] public GameObject portalLeftInstance;
+    [HideInInspector] public GameObject portalRightInstance;
+
+    // the current material attached to the portals
+    private Material portalLeftCurrentMaterial;
+    private Material portalRightCurrentMaterial;
+
+    [Header("")]
+    [SerializeField] private PortalCrosshair crosshairs;
+
+    private void Awake()
+    {
+        playerInputActions = InputManager.Instance.playerInputActions;
+
+        playerInputActions.Player.ShootPortalLeft.performed += ShootPortalLeft;
+        playerInputActions.Player.ShootPortalRight.performed += ShootPortalRight;
+    }
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        PortalMaterials();
+    }
+
+    private void ShootPortalLeft(InputAction.CallbackContext context)
+    {
+        ShootPortal(0);
+    }
+
+    private void ShootPortalRight(InputAction.CallbackContext context)
+    {
+        ShootPortal(1);
+    }
+
+    /// <summary>
+    /// Instantiates the relevant portal object within the scene
+    /// </summary>
+    /// <param name="portalID"> The ID of the portal to be Spawned (0 = left, 1 = right) </param>
+    private void ShootPortal(int portalID)
+    {
+        int x = Screen.width / 2;
+        int y = Screen.height / 2;
+
+        Ray ray = mainCamera.ScreenPointToRay(new Vector3(x, y));
+
+        if (Physics.Raycast(ray, out RaycastHit hit) && hit.transform.CompareTag("Portalable"))
+        {
+            Quaternion hitObjectRotation = Quaternion.LookRotation(hit.normal);
+
+            if (portalID == 0)
+            {
+                if (portalLeftInstance != null)
+                {
+                    Destroy(portalLeftInstance);
+                }
+
+                portalLeftInstance = Instantiate(portalLeftPrefab, hit.point, hitObjectRotation);
+            }
+            else if (portalID == 1)
+            {
+                if (portalRightInstance != null)
+                {
+                    Destroy(portalRightInstance);
+                }
+
+                portalRightInstance = Instantiate(portalRightPrefab, hit.point, hitObjectRotation);
+            }
+
+                    crosshairs.SetPortalCrosshairActive(portalID);
+        }
+    }
+
+    /// <summary>
+    /// Sets the material of the portals to either black or a render texture based on portals in scene
+    /// </summary>
+    private void PortalMaterials()
+    {
+        if (portalLeftInstance != null)
+        {
+            portalLeftCurrentMaterial = portalLeftInstance.GetComponent<Renderer>().material;
+        }
+        if (portalRightInstance != null)
+        {
+            portalRightCurrentMaterial = portalRightInstance.GetComponent<Renderer>().material;
+        }
+
+        // if left exists and right doesnt
+        if (portalLeftInstance != null && portalRightInstance == null)
+        {
+            // blue = black
+            portalLeftCurrentMaterial.color = Color.black;
+        }
+        // if right exists and left doesnt
+        if (portalRightInstance != null && portalLeftInstance == null)
+        {
+            // red = black
+            portalRightCurrentMaterial.color = Color.black;
+        }
+        // if both exist
+        if (portalRightInstance != null && portalLeftInstance != null)
+        {
+            // blue = blue
+            portalLeftCurrentMaterial = portalLeftActiveMaterial;
+            // red = red
+            portalRightCurrentMaterial = portalRightActiveMaterial;
+        }
+    }
+
+    /// <summary>
+    /// Destroys both any instances of portals in scene
+    /// </summary>
+    public void DestroyPortals()
+    {
+        if(portalLeftInstance != null)
+        {
+            Destroy(portalLeftInstance);
+        }
+        if(portalRightInstance != null)
+        {
+            Destroy(portalRightInstance);
+        }
+        crosshairs.DisableCrosshairs();
+    }
+}
